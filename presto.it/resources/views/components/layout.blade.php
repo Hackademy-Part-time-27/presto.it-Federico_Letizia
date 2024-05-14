dd5459a9f9f9a55c08b205127efad255d8ed83e2
Federico Letizia <federicoletizia12@gmail.com>
2024-05-14 20:25:09 +0200

    layout e navbar

---
+++ presto.it/resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<nav>
+    <ul>
+        <li>//logo</li>
+        <li>HOME</li>
+        <li>Contatti</li>
+        <li>Su di noi</li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav>
+
+    {{ $slot}}
+</body>
+</html>
