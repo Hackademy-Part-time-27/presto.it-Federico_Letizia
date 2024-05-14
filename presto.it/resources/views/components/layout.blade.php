0bf01a14b57425adb43a4da93fb9262d56bf842b
Federico Letizia <federicoletizia12@gmail.com>
2024-05-14 21:13:33 +0200

    home page

--- presto.it/resources/views/components/layout.blade.php
+++ presto.it/resources/views/components/layout.blade.php
@@ -3,20 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
+    <style>
+        body{
+            height: 3000px;
+        }
+    </style>
 </head>
 <body>
     
-<nav>
-    <ul>
-        <li>//logo</li>
-        <li>HOME</li>
-        <li>Contatti</li>
-        <li>Su di noi</li>
-        <li></li>
-        <li></li>
-    </ul>
-</nav>
+    <nav class="navbar navbar-expand-lg bg-primary-subtle sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-2" href="#">Presto.it</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active fs-5" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link fs-5" href="#">Contatti</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link fs-5" href="#">Chi siamo</a>
+            </li>
+        </ul>
+        </div>
+        <div>
+            <div class="d-flex text-center">
+                <button type="button" class="btn btn-info">Registrati</button>
+                <button type="button" class="btn btn-info mx-4">Accedi</button>
+            </div>
+        </div>
+    </div>
+    </nav>
 
     {{ $slot}}
 </body>
---
+++ presto.it/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+
+    <h1 class="m-5">Presto.it</h1>
+    
+    
+    
+
+
+
+
+</x-layout>
