46d96c55a7b598a49e5a8225425992f5d7b6edbb
Federico Letizia <federicoletizia12@gmail.com>
2024-05-16 23:58:59 +0200

    terminato user story 1

--- presto.it/resources/views/components/cardAnnunci.blade.php
+++
@@ -1,9 +0,0 @@
-
-<div class="card text-center" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Titolo Annuncio</h5>
-        <p class="card-text">descrizione annuncio</p>
-        <a href="#" class="btn btn-primary">Scopri di più</a>
-    </div>
-</div>
